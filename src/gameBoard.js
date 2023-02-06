22c71f82f624e6fffe28cf4fee8997ea488f3f20
wakana <wakanawardrobe@gmail.com>
2023-02-06 11:20:36 +0000

    change missedShots to be stored in an array

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -57,18 +57,10 @@ export const Gameboard = () => {
         } 
     }
     //change this to an array of missedShots, this way we have access to lots of array methods if we need to work on the missed shots data.
-    const missedShots = {
-        0: [],
-        1: [],
-        2: [],
-        3: [],
-        4: [],
-        5: [],
-        6: [],
-        7: [],
-        8: [],
-        9: [],
-    }
+
+    const missedShots = [
+        [],[],[],[],[],[],[],[],[],[]
+    ]
 
     const getMisses = () => {
         return missedShots;
