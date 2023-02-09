091c63beb838d0f87be55e6de53e2478aed7513f
wakana <wakanawardrobe@gmail.com>
2023-02-09 15:51:55 +0000

    seperate some of the attack logic into priv func

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-//We want a factory function for creating the ships that our players will use, this factory would have properties on it like the type of ship, size/length of the ship. We want methods in this ship factory that allow for the ships to take damage, for example in the instance that a ship gets clicked on, we want to add a hit counter to it. This ship and its data would be an object.
-
-//the factory should also have a method on it to check if the players ship is sunk. This would be a boolean value that checks the number of hits it has recieved against the length of the ship. 
-
 import { Ship } from "./shipModule";
 import { Gameboard } from "./gameBoard";
+import { Player } from "./player";
 
-const playerBoard = Gameboard();
+const gameLoop = () => {
 
-playerBoard.placeShip(Ship(4), [0,2]);
-playerBoard.recieveAttack([0,3]);
-playerBoard.recieveAttack([0,2]);
-console.log(playerBoard.getBoard());
-console.log(playerBoard.getBoard()[0][4].getDamageRecieved());
+    const playerBoard = Gameboard();
+    const computerBoard = Gameboard();
+
+    const player = Player('V', computerBoard);
+    const computer = Player('comp', playerBoard, true);
+
+    console.log(playerBoard.getBoard());
+}
--- src/player.js
+++ src/player.js
@@ -2,32 +2,25 @@ import { Gameboard } from "./gameBoard"
 
 export const Player = (name, opponentBoard, isCPU = false) => {
 //factory for our different players (in the case of this game it would be the player and the computer) would take a name, and the opposing players board. So if we make a player it would take in the computers board.
-    const attack = (coordinate) => {
-        if (isCPU) {
-            
-            const validPositions = opponentBoard.getValidPositions();
-            
-            let cpuCoordinate = validPositions[Math.floor(Math.random() * validPositions.length)];
 
-            opponentBoard.recieveAttack(cpuCoordinate);
-
-            return cpuCoordinate;
-        }
-        //check if isCPU is true, if it is we want to calculate a random coordinate on the boardand use that for the board to recieveAttack. 
-
-        //player attack coordinate can be passed into here.
-        opponentBoard.recieveAttack(coordinate);
-
-    }
-
-    const randomAttack = () => {
+    const caculationRandomPosition = () => {
         const validPositions = opponentBoard.getValidPositions();
             
-        let cpuCoordinate = validPositions[Math.floor(Math.random() * validPositions.length)];
+        const position = validPositions[Math.floor(Math.random() * validPositions.length)];
 
-        opponentBoard.recieveAttack(cpuCoordinate);
+        return position;
+    }
 
-        return cpuCoordinate;
+    const attack = (coordinate) => {
+        if (isCPU) {
+
+            const randomPosition = caculationRandomPosition();
+            opponentBoard.recieveAttack(randomPosition);
+
+            return randomPosition;
+        }
+        opponentBoard.recieveAttack(coordinate);
+
     }
 
     return { attack }
