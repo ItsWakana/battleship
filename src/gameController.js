3dbe63aa989bae5cf655e4349f0723f202508379
itswakana <100365678+ItsWakana@users.noreply.github.com>
2023-03-07 02:11:55 +0000

    fix gameboard center align and max-width

--- src/gameController.js
+++ src/gameController.js
@@ -114,16 +114,16 @@ export const gameController = () => {
 
         if (game.computerBoard.getLastHit()['ship']) {
             audioSetup.playRandomHitSound();
-            // await delay(delayTime.waitForSound);
+            await delay(delayTime.waitForSound);
             view.updateBoard(game.computerBoard.getBoard(), true);  
             view.handlePlayerHitState(coordinate);
             return;
         }
         audioSetup.playRandomMissSound()
-        // await delay(delayTime.waitForSound);
+        await delay(delayTime.waitForSound);
         view.updateBoard(game.computerBoard.getBoard(), true);  
         view.DOMHelper.speechBubbleText(view.response.playerMissResponse());
-        // await delay(delayTime.waitForSpeech);
+        await delay(delayTime.waitForSpeech);
         view.handlePlayerMissState();
         await delay(3000);
         handleComputerTurn();
